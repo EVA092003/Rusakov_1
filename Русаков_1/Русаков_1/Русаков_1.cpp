449f62cd768e5f16a7cdd5ab5d4dc7ee62a769a2
1917Lenin1917 <ltv.maxym@gmail.com>
2022-12-02 23:41:01 +0300

    Added working main. Fixed nops. Fixed memory leaks

--- Русаков_1/Русаков_1/Русаков_1.cpp
+++ Русаков_1/Русаков_1/Русаков_1.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+﻿#include <chrono>
+#include <iostream>
 
 
 #include "QueuePtr1head.h"
@@ -6,19 +7,20 @@
 
 using namespace std;
 
-void mergeSort4queuePtr1head(Adapter4queuePtr1head* a, int l, int r)
+void mergeSort4queuePtr1head(Adapter4queuePtr1head* a, int l, int r, uint64_t* nop)
 {
     //cout << endl; a->display();
     if (l == r) return; // границы сомкнулись
     int mid = (l + r) / 2; // определяем середину последовательности
     // и рекурсивно вызываем функцию сортировки для каждой половины
-    mergeSort4queuePtr1head(a, l, mid);
-    mergeSort4queuePtr1head(a, mid + 1, r);
+    mergeSort4queuePtr1head(a, l, mid, nop);
+    mergeSort4queuePtr1head(a, mid + 1, r, nop);
     int i = l;  // начало первого пути
     int j = mid + 1; // начало второго пути
     //int* tmp = (int*)malloc(r * sizeof(int)); // дополнительный массив
     Adapter4queuePtr1head* tmp = new Adapter4queuePtr1head();
-    for (int i = 0; i < r + 1; i++)
+    tmp->setNOP(nop);
+    for (int k = 0; k < r + 1; k++)
         tmp->push(-1);
 
     for (int step = 0; step < r - l + 1; step++) // для всех элементов дополнительного массива
@@ -44,10 +46,13 @@ void mergeSort4queuePtr1head(Adapter4queuePtr1head* a, int l, int r)
     for (int step = 0; step < r - l + 1; step++)
         //a[l + step] = tmp[step];
         a->setElement(l + step, tmp->getElement(step));
+
+    delete tmp;
 }
 
-void  example_mergeSort4queuePtr1head() {
+void  example_mergeSort4queuePtr1head(uint64_t* nop) {
     Adapter4queuePtr1head* q1 = new Adapter4queuePtr1head();
+    q1->setNOP(nop);
     q1->push(6);
     q1->push(2);
     q1->push(3);
@@ -57,7 +62,7 @@ void  example_mergeSort4queuePtr1head() {
     int size = q1->size();
     cout << "Размер очереди:: " << size << endl;
     cout << endl; q1->display();
-    mergeSort4queuePtr1head(q1, 0, size - 1);
+    mergeSort4queuePtr1head(q1, 0, size - 1, nop);
     cout << endl << "Отсортированный: " << endl;
     q1->display();
 
@@ -130,7 +135,7 @@ void example_work_adapter4queuePtr1head() {
 //            q1->pop();
 //    }
 //};
-void example_work_queueMas(int* _nop) {
+void example_work_queueMas(uint64_t* _nop) {
     QueuePtr1head* q1 = new QueuePtr1head();
     q1->setNOP(_nop);
     q1->push(10);
@@ -158,7 +163,7 @@ void example_work_queueMas(int* _nop) {
     cout << endl << *q1->getNOP() << endl;
 }
 
-void example_work_adapter4queueMas(int* _nop) {
+void example_work_adapter4queueMas(uint64_t* _nop) {
     Adapter4queuePtr1head* q1 = new Adapter4queuePtr1head();
     q1->setNOP(_nop);
     q1->push(1000);
@@ -177,16 +182,37 @@ void example_work_adapter4queueMas(int* _nop) {
     cout << endl << *q1->getNOP() << endl;
 }
 
+void work(int size)
+{
+    typedef std::chrono::high_resolution_clock Clock;
+
+
+    uint64_t* nop = new uint64_t{ 0 };
+    Adapter4queuePtr1head* a = new Adapter4queuePtr1head;
+    a->setNOP(nop);
+
+    for (int i = 0; i < size; i++)
+        a->push(rand() % 20);
+
+    auto start = Clock::now();
+    mergeSort4queuePtr1head(a, 0, size - 1, nop);
+    auto end = Clock::now();
+
+    auto time_ms = chrono::duration_cast<chrono::milliseconds>(Clock::now() - start).count();
+    std::cout << size << " " << time_ms / 1000.0f << " " << *nop << "\n";
+
+    delete a;
+    delete nop;
+}
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    // var 43
-    //Указатели	Очередь с 1 головой	Естественное двухпутевое слияние
-    //example_work_QueuePtr1head();
-    //example_work_adapter4queuePtr1head();
-    //example_work_quickSort();
-    int* nop = new int;
-    *nop = 0;
-    example_work_queueMas(nop);
+    srand(time(NULL));
+
+    for (int i = 0; i < 10; i++)
+    {
+        work((i + 1) * 30);
+
+    }
 }
